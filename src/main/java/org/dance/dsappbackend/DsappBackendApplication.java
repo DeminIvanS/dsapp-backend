51bc084f6a1518b5a301e9a509c7a2390ae10cd6
Ivan <taim-off@mail.ru>
2026-06-21 19:37:43 +0300

    hello world + docker compose and init.sql

---
+++ src/main/java/org/dance/dsappbackend/DsappBackendApplication.java
@@ -0,0 +1,13 @@
+package org.dance.dsappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsappBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsappBackendApplication.class, args);
+    }
+
+}
